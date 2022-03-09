d1e691c992304c7b33b3efd3d74bc3ba5c8a7d2b
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-03-09 13:09:24 -0700

    1st commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let child1 = document.querySelector('.child-1');
+
+child1.innerText = 'CHILD 1';
+child1.className = 'child-2';
+
+let child3 = document.querySelector('.child-3');
+
+child3.style.border = '2px solid blue';
+child3.style.background = 'yellow';
+
+let image = document.querySelector('img');
+
+image.style.border = '2px solid blue';
+
+image.src = 'https://www.vets4pets.com/siteassets/dental-webheaders-dog---oct-20.jpg?w=585&scale=down'
+image.style.height = '500px'
+
+let header = document.querySelector('h1');
+header.innerText = 'DOM Introduction!!!!!';
+
+let child4 = document.querySelector('#child-4');
+child4.style.border = '2px solid green';
+
+console.log(`dope!`);
